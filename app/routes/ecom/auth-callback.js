3718cba3df096a63340169546985df3caf2f5e9a
igorsfreitas <igor_s_freitas@hotmail.com>
2020-02-07 18:45:38 -0300

    feat(firestore-support): minor changes

--- app/routes/ecom/auth-callback.js
+++ app/routes/ecom/auth-callback.js
@@ -1,7 +1,5 @@
 'use strict'
 
-// log on files
-const logger = require('console-files')
 const { ecomAuth } = require('ecomplus-app-sdk')
 module.exports = () => {
   return (req, res) => {
@@ -11,35 +9,27 @@ module.exports = () => {
     // https://github.com/ecomclub/ecomplus-app-sdk
     ecomAuth.then(appSdk => {
       appSdk.handleCallback(storeId, req.body)
-      .then(({ isNew, authenticationId }) => {
-        if(isNew){
-          appSdk.saveProcedures(storeId, procedures)
-        }
-        // authentication tokens were updated
-        res.status(204)
-        res.end()
-      })
-
-      .catch(err => {
-        if (typeof err.code === 'string' && !err.code.startsWith('SQLITE_CONSTRAINT')) {
-          // debug SQLite errors
-          logger.error(err)
-        }
-        res.status(500)
-        let { message } = err
-        res.send({
-          error: 'auth_callback_error',
-          message
+        .then(({ isNew, authenticationId }) => {
+          if (isNew) {
+            appSdk.saveProcedures(storeId, procedures)
+          }
+          // authentication tokens were updated
+          res.status(204)
+          res.end()
         })
-      })
+
+        .catch(err => {
+          if (typeof err.code === 'string' && !err.code.startsWith('SQLITE_CONSTRAINT')) {
+            // debug SQLite errors
+            console.error(err)
+          }
+          res.status(500)
+          const { message } = err
+          res.send({
+            error: 'auth_callback_error',
+            message
+          })
+        })
     })
-    
-    ecomAuth.catch(err => {
-      logger.error(err)
-      setTimeout(() => {
-        process.exit(1)
-      }, 1100)
-    })
-    
   }
 }
