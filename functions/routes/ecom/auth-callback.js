0cf2ddee447fed6e00957f0691ddd8ae10fd5bca
Leonardo Matos <leomp120894@gmail.com>
2020-02-11 19:41:37 -0300

    fix(routes): fix handling appSdk (param)

--- functions/routes/ecom/auth-callback.js
+++ functions/routes/ecom/auth-callback.js
@@ -1,32 +1,30 @@
 'use strict'
 
-const { ecomAuth } = require('ecomplus-app-sdk')
-module.exports = () => {
-  return (req, res) => {
-    const { storeId } = req
-    const procedures = require('./../../lib/store-api/procedures')
-    // handle callback with E-Com Plus app SDK
-    // https://github.com/ecomclub/ecomplus-app-sdk
-    ecomAuth.then(appSdk => {
-      appSdk.handleCallback(storeId, req.body)
-        .then(({ isNew, authenticationId }) => {
-          if (isNew) {
-            appSdk.saveProcedures(storeId, procedures)
-          }
-          // authentication tokens were updated
-          res.status(204)
-          res.end()
-        })
+// E-Com Plus Procedures to register
+const procedures = require('./../../lib/store-api/procedures')
 
-        .catch(err => {
-          console.error(err)
-          res.status(500)
-          const { message } = err
-          res.send({
-            error: 'auth_callback_error',
-            message
-          })
-        })
+module.exports = ({ appSdk }) => (req, res) => {
+  const { storeId } = req
+
+  // handle callback with E-Com Plus app SDK
+  // https://github.com/ecomclub/ecomplus-app-sdk
+  appSdk.handleCallback(storeId, req.body)
+    .then(({ isNew, authenticationId }) => {
+      if (isNew) {
+        appSdk.saveProcedures(storeId, procedures)
+      }
+      // authentication tokens were updated
+      res.status(204)
+      res.end()
     })
-  }
+
+    .catch(err => {
+      console.error(err)
+      res.status(500)
+      const { message } = err
+      res.send({
+        error: 'auth_callback_error',
+        message
+      })
+    })
 }
