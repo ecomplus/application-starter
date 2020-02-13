30ac36d106f4103e03ca9f134018c019ada9248d
igorsfreitas <igor_s_freitas@hotmail.com>
2020-02-12 23:39:38 -0300

    docs(app): set base-uri for each module on app.js

--- functions/routes/index.js
+++ functions/routes/index.js
@@ -4,7 +4,18 @@ const baseApp = require('./../assets/app.json')
 const functions = require('firebase-functions')
 const appBaseUrl = functions.config().app.base_uri
 
-baseApp.auth_callback_uri = baseApp.auth_callback_uri.replace(/<base-uri>/i, appBaseUrl)
+if (appBaseUrl) {
+  baseApp.auth_callback_uri = baseApp.auth_callback_uri.replace(/<base-uri>/i, appBaseUrl)
+}
+
+if (baseApp.modules && baseApp.modules.length > 0) {
+  const modules = Object.keys(baseApp.modules)
+
+  modules.forEach((module) => {
+    baseApp.modules[module].endpoint = baseApp.modules[module].endpoint.replace(/<base-uri>/i, appBaseUrl)
+  })
+}
+
 module.exports = (req, res) => {
   res.send(baseApp)
 }
