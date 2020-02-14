a3c71005320551b3110037e461b34e90ad6ba1b9
igorsfreitas <igor_s_freitas@hotmail.com>
2020-02-13 23:38:45 -0300

    fix(auth-callback): fix to saveProcedures only if not isNew

--- functions/routes/ecom/auth-callback.js
+++ functions/routes/ecom/auth-callback.js
@@ -9,9 +9,24 @@ module.exports = ({ appSdk }) => (req, res) => {
   // handle callback with E-Com Plus app SDK
   // https://github.com/ecomclub/ecomplus-app-sdk
   appSdk.handleCallback(storeId, req.body)
-    .then(({ isNew, authenticationId }) => {
-      if (isNew) {
-        appSdk.saveProcedures(storeId, procedures)
+    .then(({ isNew, authenticationId, settedUp }) => {
+      if (!isNew && !settedUp) {
+        appSdk.getAuth(storeId, authenticationId).then(auth => {
+          appSdk.saveProcedures(storeId, procedures).then(() => {
+            if (auth.doc) {
+              auth.doc(authenticationId).set({ setted_up: true }, { merge: true })
+                .catch(err => {
+                  console.error(err)
+                  res.status(500)
+                  const { message } = err
+                  res.send({
+                    error: 'auth_callback_error',
+                    message
+                  })
+                })
+            }
+          })
+        })
       }
       // authentication tokens were updated
       res.status(204)
