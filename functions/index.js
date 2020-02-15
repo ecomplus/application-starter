ce107b74cde375e875a85cc3ba0cc6a73740785d
Leonardo Matos <leomp120894@gmail.com>
2020-02-15 17:25:43 -0300

    feat(server): start reading env options, handle operator token

--- functions/index.js
+++ functions/index.js
@@ -2,29 +2,28 @@
 
 'use strict'
 
+const { functionName, operatorToken, app } = require('./__env')
+
 // Firebase SDKs to setup cloud functions and access Firestore database
 const admin = require('firebase-admin')
 const functions = require('firebase-functions')
-const functionName = 'ecomApp'
 admin.initializeApp()
 
 // web server with Express
 const express = require('express')
 const bodyParser = require('body-parser')
-const app = express()
+const server = express()
 const router = express.Router()
 const routes = './routes'
 
 // handle app authentication to Store API
 // https://github.com/ecomclub/ecomplus-app-sdk
-// disable set interval (no daemons on cloud functions)
-process.env.ECOM_AUTH_UPDATE_INTERVAL = 'disabled'
 const { ecomServerIps, setup } = require('ecomplus-app-sdk')
 
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
+server.use(bodyParser.urlencoded({ extended: false }))
+server.use(bodyParser.json())
 
-app.use((req, res, next) => {
+server.use((req, res, next) => {
   if (req.url.startsWith('/ecom/')) {
     // get E-Com Plus Store ID from request header
     req.storeId = parseInt(req.get('x-store-id'), 10)
@@ -37,10 +36,15 @@ app.use((req, res, next) => {
       }
     }
 
-    // on production check if request is comming from E-Com Plus servers
-    const clientIp = req.get('x-forwarded-for') || req.connection.remoteAddress
-    if (process.env.NODE_ENV === 'production' && ecomServerIps.indexOf(clientIp) === -1) {
-      return res.status(403).send('Who are you? Unauthorized IP address')
+    if (process.env.NODE_ENV !== 'development') {
+      // check for operator token
+      if (operatorToken !== req.get('x-operator-token')) {
+        // last check for IP address from E-Com Plus servers
+        const clientIp = req.get('x-forwarded-for') || req.connection.remoteAddress
+        if (process.env.NODE_ENV !== 'development' && ecomServerIps.indexOf(clientIp) === -1) {
+          return res.status(403).send('Who are you? Unauthorized IP address')
+        }
+      }
     }
   }
 
@@ -51,15 +55,27 @@ app.use((req, res, next) => {
 
 router.get('/', require(`${routes}/`))
 
-// setup ecomAuth client with Firestore instance
-setup(null, true, admin.firestore()).then(appSdk => {
-  // base routes for E-Com Plus Store API
-  ;['auth-callback', 'webhook'].forEach(endpoint => {
-    const filename = `/ecom/${endpoint}`
-    router.post(filename, require(`${routes}${filename}`)({ appSdk, admin }))
+// base routes for E-Com Plus Store API
+;['auth-callback', 'update-tokens', 'webhook'].forEach(endpoint => {
+  const filename = `/ecom/${endpoint}`
+  router.post(filename, (req, res) => {
+    // first disable set interval (no daemons on cloud functions)
+    process.env.ECOM_AUTH_UPDATE_INTERVAL = 'disabled'
+
+    // setup ecomAuth client with Firestore instance
+    setup(null, true, admin.firestore()).then(appSdk => {
+      require(`${routes}${filename}`)({ appSdk, admin }, req, res)
+    }).catch(err => {
+      console.error(err)
+      res.status(500)
+      res.send({
+        error: 'SETUP',
+        message: 'Can\'t setup `ecomAuth`, check Firebase console registers'
+      })
+    })
   })
 })
 
-app.use(router)
-exports[functionName] = functions.https.onRequest(app)
-console.log(`Starting E-Com Plus app with Cloud Function '${functionName}'`)
+server.use(router)
+exports[functionName] = functions.https.onRequest(server)
+console.log(`Starting '${app.title}' E-Com Plus app with Cloud Function '${functionName}'`)
