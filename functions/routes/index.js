aae8ffefa2068bf9de040462547afac46a9c57f4
Leonardo Matos <leomp120894@gmail.com>
2020-02-15 17:28:04 -0300

    refactor(home): merge env config and fix showing app info on root route

--- functions/routes/index.js
+++ functions/routes/index.js
@@ -1,21 +1,27 @@
 'use strict'
 
+const { baseUri, app } = require('./../__env')
+
+// mocked app body
 const baseApp = require('./../assets/app.json')
-const functions = require('firebase-functions')
-const appBaseUrl = functions.config().app.base_uri
 
-if (appBaseUrl) {
-  baseApp.auth_callback_uri = baseApp.auth_callback_uri.replace(/<base-uri>/i, appBaseUrl)
+if (app) {
+  // merge with app from Function config
+  Object.assign(baseApp, app)
 }
-
-if (baseApp.modules && baseApp.modules.length > 0) {
-  const modules = Object.keys(baseApp.modules)
-
-  modules.forEach((module) => {
-    baseApp.modules[module].endpoint = baseApp.modules[module].endpoint.replace(/<base-uri>/i, appBaseUrl)
-  })
+if (baseUri) {
+  // replace `<base-uri>` mask to fix endpoints on app body
+  const replaceBaseUri = mask => mask.replace(/<base-uri>/i, baseUri)
+  baseApp.auth_callback_uri = replaceBaseUri(baseApp.auth_callback_uri)
+  if (baseApp.modules && baseApp.modules.length > 0) {
+    const modules = Object.keys(baseApp.modules)
+    modules.forEach(modName => {
+      baseApp.modules[modName].endpoint = replaceBaseUri(baseApp.modules[module].endpoint)
+    })
+  }
 }
 
 module.exports = (req, res) => {
+  // showing app info on root route
   res.send(baseApp)
 }
