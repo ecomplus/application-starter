cea7c7e847a6c412dcc94f91de3d13e7f3467999
Leonardo Matos <leomp120894@gmail.com>
2020-02-16 17:28:22 -0300

    build(firebase): setup node script to serve/deploy with firebase

---
+++ firebase.js
@@ -0,0 +1,48 @@
+const {
+  FIREBASE_COMMAND,
+  FIREBASE_TOKEN,
+  FIREBASE_PROJECT_ID,
+  APP_TITLE,
+  SERVER_OPERATOR_TOKEN,
+  SERVER_BASE_URI
+} = process.env
+
+if (!FIREBASE_TOKEN || !SERVER_OPERATOR_TOKEN) {
+  const env = require('dotenv').config()
+  if (env.error) {
+    throw env.error
+  }
+}
+
+let project = FIREBASE_PROJECT_ID
+if (!project) {
+  try {
+    const firebaserc = require('./.firebaserc')
+    project = firebaserc.projects.default
+  } catch (e) {
+    project = 'ecom-app'
+  }
+}
+
+const client = require('firebase-tools')
+
+let config = `server.operatorToken="${SERVER_OPERATOR_TOKEN}"`
+if (SERVER_BASE_URI) {
+  config += ` server.baseUri="${SERVER_BASE_URI}"`
+}
+if (APP_TITLE) {
+  config += ` app.title="${APP_TITLE}"`
+}
+
+client.functions.config.set(config).then(() => {
+  return client[FIREBASE_COMMAND || 'serve']({
+    project,
+    only: 'functions',
+    token: FIREBASE_TOKEN,
+    force: true
+  }).then(() => {
+    console.log(`Deployed with success to Firebase project '${project}'`)
+  })
+}).catch(err => {
+  throw err
+})
