d4b61fab427aefdb2ac485d90eb1abe15d6aafc6
Leonardo Matos <leomp120894@gmail.com>
2020-02-18 01:33:08 -0300

    feat(home): set version and slug from root package, fix with uris

--- functions/routes/index.js
+++ functions/routes/index.js
@@ -1,24 +1,35 @@
 'use strict'
 
-const { baseUri, app } = require('./../__env')
+const { baseUri, app, pkg } = require('./../__env')
 
 // mocked app body
-const baseApp = require('./../assets/app.json')
+const baseApp = require('./../ecom-app.json')
 
 if (app) {
   // merge with app from Function config
   Object.assign(baseApp, app)
 }
+
 if (baseUri) {
-  // replace `<base-uri>` mask to fix endpoints on app body
-  const replaceBaseUri = mask => mask.replace(/<base-uri>/i, baseUri)
-  baseApp.auth_callback_uri = replaceBaseUri(baseApp.auth_callback_uri)
-  if (baseApp.modules && baseApp.modules.length > 0) {
-    const modules = Object.keys(baseApp.modules)
-    modules.forEach(modName => {
-      baseApp.modules[modName].endpoint = replaceBaseUri(baseApp.modules[module].endpoint)
+  // fix endpoints with current function URIs on app body
+  if (!baseApp.auth_callback_uri) {
+    baseApp.auth_callback_uri = `${baseUri}/ecom/auth-callback`
+  }
+  if (baseApp.modules) {
+    Object.keys(baseApp.modules).forEach(modName => {
+      if (!baseApp.modules[modName].endpoint) {
+        baseApp.modules[modName].endpoint = `${baseUri}/ecom/modules/${modName}`
+      }
     })
   }
+}
+
+// set version and slug from root package
+if (!baseApp.version && pkg.version) {
+  baseApp.version = pkg.version
+}
+if (!baseApp.slug && pkg.name) {
+  baseApp.slug = pkg.name.replace('/', '-').replace(/[^0-9a-z-]/ig, '')
 }
 
 module.exports = (req, res) => {
