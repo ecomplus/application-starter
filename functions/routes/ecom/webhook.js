53949d47c9cda830029537d8cfe67cd9b80f538d
Leonardo Matos <leomp120894@gmail.com>
2020-02-20 12:56:32 -0300

    chore(webhook): minir update default code and comments instructions

--- functions/routes/ecom/webhook.js
+++ functions/routes/ecom/webhook.js
@@ -7,26 +7,31 @@ const ECHO_SKIP = 'SKIP'
 const ECHO_API_ERROR = 'STORE_API_ERR'
 
 exports.post = ({ appSdk }, req, res) => {
+  // receiving notification from Store API
   const { storeId } = req
-  /*
-  Treat E-Com Plus trigger body here
-  // https://developers.e-com.plus/docs/api/#/store/triggers/
+
+  /**
+   * Treat E-Com Plus trigger body here
+   * Ref.: https://developers.e-com.plus/docs/api/#/store/triggers/
+   */
   const trigger = req.body
-  */
 
   // get app configured options
   getAppData({ appSdk, storeId })
 
-    .then(configObj => {
-      /* Do the stuff */
-
-      // example only
-      if (configObj.ignore_trigger_x) {
+    .then(appData => {
+      if (
+        Array.isArray(appData.ignore_triggers) &&
+        appData.ignore_triggers.indexOf(trigger.resource) > -1
+      ) {
         // ignore current trigger
         const err = new Error()
         err.name = SKIP_TRIGGER_NAME
         throw err
       }
+
+      /* DO YOUR CUSTOM STUFF HERE */
+
       // all done
       res.send(ECHO_SUCCESS)
     })
@@ -36,7 +41,7 @@ exports.post = ({ appSdk }, req, res) => {
         // trigger ignored by app configuration
         res.send(ECHO_SKIP)
       } else {
-        // logger.error(err)
+        // console.error(err)
         // request to Store API with error response
         // return error status code
         res.status(500)
