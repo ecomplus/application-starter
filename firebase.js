90dc05defe03dcc92cb6c76d59a99ade0d5361ef
Leonardo Matos <leomp120894@gmail.com>
2020-02-21 11:15:48 -0300

    build(firebase): pretty logs after deploy

--- firebase.js
+++ firebase.js
@@ -31,17 +31,25 @@ if (SERVER_BASE_URI) {
 }
 
 client.functions.config.set(config, { project })
+  .then(() => client.deploy({
+    project,
+    only: 'functions',
+    token: FIREBASE_TOKEN,
+    force: true
+  }))
+
   .then(() => {
-    return client.deploy({
-      project,
-      only: 'functions',
-      token: FIREBASE_TOKEN,
-      force: true
-    }).then(() => {
-      console.log(`Deployed with success to Firebase project '${project}'`)
-      console.log(SERVER_BASE_URI || `https://us-central1-${project}.cloudfunctions.net/app/`)
-    })
+    console.log(
+      '\x1b[32m%s\x1b[0m',
+      `\nDeployed with success to Firebase project '${project}'`
+    )
+    console.log(
+      '\x1b[35m%s\x1b[0m',
+      `\nBase URI: ${(SERVER_BASE_URI || `https://us-central1-${project}.cloudfunctions.net/app/`)}`
+    )
+    console.log()
   })
+
   .catch(err => {
     console.error(err)
     process.exit(1)
