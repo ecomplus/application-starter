ec388a8cf47603fd15c448d2aae53623cd870c62
Talisson Trindade <talissonf@gmail.com>
2021-02-13 19:46:37 -0300

    feat(modules): start examples and mocks for discounts and payment modules (#66)
    * docs(create-transaction): examples for using the module
    
    * docs: examples for using the module
    
    * docs(apply-discount): exemple for using the module
    
    * Update functions/routes/ecom/modules/apply-discount.js
    
    Co-authored-by: Leonardo Matos <leomp120894@gmail.com>
    
    * Update functions/routes/ecom/modules/apply-discount.js
    
    Co-authored-by: Leonardo Matos <leomp120894@gmail.com>
    
    * Update functions/routes/ecom/modules/apply-discount.js
    
    Co-authored-by: Leonardo Matos <leomp120894@gmail.com>
    
    * Update functions/routes/ecom/modules/apply-discount.js
    
    Co-authored-by: Leonardo Matos <leomp120894@gmail.com>
    
    * Update functions/routes/ecom/modules/apply-discount.js
    
    Co-authored-by: Leonardo Matos <leomp120894@gmail.com>
    
    * chore: example for using available_extra_discount
    
    * Update functions/routes/ecom/modules/apply-discount.js
    
    Co-authored-by: Leonardo Matos <leomp120894@gmail.com>
    
    * Update functions/routes/ecom/modules/apply-discount.js
    
    Co-authored-by: Leonardo Matos <leomp120894@gmail.com>
    
    * Update functions/routes/ecom/modules/create-transaction.js
    
    Co-authored-by: Leonardo Matos <leomp120894@gmail.com>
    
    * Update functions/routes/ecom/modules/create-transaction.js
    
    Co-authored-by: Leonardo Matos <leomp120894@gmail.com>
    
    * Update functions/routes/ecom/modules/create-transaction.js
    
    Co-authored-by: Leonardo Matos <leomp120894@gmail.com>
    
    * Update functions/routes/ecom/modules/apply-discount.js
    
    Co-authored-by: Leonardo Matos <leomp120894@gmail.com>
    
    Co-authored-by: Leonardo Matos <leomp120894@gmail.com>

--- functions/routes/ecom/modules/list-payments.js
+++ functions/routes/ecom/modules/list-payments.js
@@ -1,3 +1,42 @@
 exports.post = ({ appSdk }, req, res) => {
-  res.send({})
+  /**
+   * Requests coming from the modules receive a hydrated body with two objects, `params` and application`.
+   * In `application` is a copy of your application installed by the merchant, including the properties` data` and `hidden_data`.
+   * JSON Schema of the list_payments module
+   * `params`: https://apx-mods.e-com.plus/api/v1/list_payments/schema.json?store_id=100
+   * `response`: https://apx-mods.e-com.plus/api/v1/list_payments/response_schema.json?store_id=100
+   */
+  const { params, application } = req.body
+  const { storeId } = req
+
+  // merge all app options configured by merchant
+  const appData = Object.assign({}, application.data, application.hidden_data)
+
+  // setup basic required response object
+  // Must follow schema: https://apx-mods.e-com.plus/api/v1/list_payments/response_schema.json?store_id=100
+  const response = {
+    payment_gateways: []
+  }
+
+  /* DO THE STUFF HERE TO FILL RESPONSE OBJECT WITH PAYMENT GATEWAYS */
+  /* 
+    // eg;
+    response.payment_gateways.push({
+      intermediator: {
+        code: 'paupay',
+        link: 'https://www.paupay.com.br',
+        name: 'paupay'
+      },
+      payment_url: 'https://www.paupay.com.br/',
+      type: 'payment',
+      payment_method: {
+        code: 'banking_billet',
+        name: 'Boleto Bancário'
+      },
+      label: 'Boleto Bancário',
+      expiration_date: appData.banking_billet.expiration_date || 14
+    })
+  */
+
+  res.send(response)
 }
