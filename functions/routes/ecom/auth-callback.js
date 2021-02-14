8000342c62fad45a823c0b50f09ff978043c3493
Leonardo Matos <leomp120894@gmail.com>
2021-02-14 00:40:31 -0300

    docs(auth-callback): sample (comments) for app self procedure

--- functions/routes/ecom/auth-callback.js
+++ functions/routes/ecom/auth-callback.js
@@ -31,6 +31,17 @@ exports.post = ({ appSdk }, req, res) => {
           if (!row.setted_up) {
             console.log(`Try saving procedures for store #${storeId}`)
 
+            /**
+             * You may want to be notified when app "self" data is edited:
+
+            procedures[0].triggers.push({
+              resource: 'applications',
+              resource_id: row.application_id,
+              field: 'data'
+            })
+
+             */
+
             // must save procedures once only
             return appSdk.saveProcedures(storeId, procedures, auth)
               .then(() => docRef.set({ setted_up: true }, { merge: true }))
