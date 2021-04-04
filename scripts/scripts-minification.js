e57811d789479ee3feac3f6857c72e270d37c968
Leonardo Matos <leomp120894@gmail.com>
2021-04-04 15:14:18 -0300

    build(scripts-minification): also handle functions assets dir

--- scripts/scripts-minification.js
+++ scripts/scripts-minification.js
@@ -2,17 +2,19 @@ const path = require('path')
 const fs = require('fs')
 const uglifyJS = require('uglify-js')
 
-const hostingPath = path.resolve(__dirname, '../hosting')
-fs.readdirSync(hostingPath).forEach(file => {
-  if (file.endsWith('.js') && !file.endsWith('.min.js')) {
-    const filePath = path.resolve(hostingPath, file)
-    if (filePath) {
-      const minify = uglifyJS.minify(fs.readFileSync(filePath, 'utf8'))
-      if (minify.code) {
-        fs.writeFileSync(filePath.replace(/.js$/, '.min.js'), minify.code, 'utf8')
-      } else {
-        console.error(minify)
+;['hosting', 'functions/assets'].forEach(dir => {
+  const scriptsPath = path.resolve(__dirname, '..', dir)
+  fs.readdirSync(scriptsPath).forEach(file => {
+    if (file.endsWith('.js') && !file.endsWith('.min.js')) {
+      const filePath = path.resolve(scriptsPath, file)
+      if (filePath) {
+        const minify = uglifyJS.minify(fs.readFileSync(filePath, 'utf8'))
+        if (minify.code) {
+          fs.writeFileSync(filePath.replace(/.js$/, '.min.js'), minify.code, 'utf8')
+        } else {
+          console.error(minify)
+        }
       }
     }
-  }
+  })
 })
