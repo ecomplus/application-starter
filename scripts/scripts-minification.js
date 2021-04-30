5016e3676c17729f07e8733e7d7a0b6a20f3ae2f
Leonardo Matos <leomp120894@gmail.com>
2021-04-30 12:51:18 -0300

    build(scripts-minification): minify js assets to dist folder

--- scripts/scripts-minification.js
+++ scripts/scripts-minification.js
@@ -4,13 +4,19 @@ const uglifyJS = require('uglify-js')
 
 ;['hosting', 'functions/assets'].forEach(dir => {
   const scriptsPath = path.resolve(__dirname, '..', dir)
+  const distPath = path.resolve(scriptsPath, 'dist')
+  if (!fs.existsSync(distPath)) {
+    fs.mkdirSync(distPath)
+  }
+
   fs.readdirSync(scriptsPath).forEach(file => {
     if (file.endsWith('.js') && !file.endsWith('.min.js')) {
       const filePath = path.resolve(scriptsPath, file)
       if (filePath) {
         const minify = uglifyJS.minify(fs.readFileSync(filePath, 'utf8'))
         if (minify.code) {
-          fs.writeFileSync(filePath.replace(/.js$/, '.min.js'), minify.code, 'utf8')
+          const outputPath = filePath.replace(/\/([^/]+).js$/, '/dist/$1.min.js')
+          fs.writeFileSync(outputPath, minify.code, 'utf8')
         } else {
           console.error(minify)
         }
