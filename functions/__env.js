9e53963aa95d79b48fe63528f3f41947f619e9e9
Leonardo Matos <leomp120894@gmail.com>
2021-05-07 05:47:20 -0300

    fix(env): try both GCP_PROJECT and GCLOUD_PROJECT (obsolete)

--- functions/__env.js
+++ functions/__env.js
@@ -1,5 +1,6 @@
 // setup server and app options from Functions config (and mocks)
-const { GCLOUD_PROJECT } = process.env
+const { GCP_PROJECT, GCLOUD_PROJECT } = process.env
+const projectId = GCP_PROJECT || GCLOUD_PROJECT
 const { pkg, server } = require('firebase-functions').config()
 const functionName = server.functionName || 'app'
 
@@ -7,8 +8,8 @@ module.exports = {
   functionName,
   operatorToken: server && server.operator_token,
   baseUri: (server && server.base_uri) ||
-    `https://us-central1-${GCLOUD_PROJECT}.cloudfunctions.net/${functionName}`,
-  hostingUri: `https://${GCLOUD_PROJECT}.web.app`,
+    `https://us-central1-${projectId}.cloudfunctions.net/${functionName}`,
+  hostingUri: `https://${projectId}.web.app`,
   pkg: {
     ...pkg
   }
